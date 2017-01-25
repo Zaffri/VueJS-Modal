86d95968754c5ec28895a0f237a69d520ee6832c
Steven Morrison <stevenmorrimorrison@hotmail.co.uk>
2017-01-25 19:51:33 +0000

    SM: Props data added to component.

--- example/js/app.js
+++ example/js/app.js
@@ -1,15 +1,22 @@
 var ZaffriModal = Vue.component('zaffri-modal', {
-    template: "#zaffri-modal-template"
+    template: "#zaffri-modal-template",
+    props: ['data']
 });
 
 var app = new Vue({
     el: "#app",
     data: {
-         modalVisible: false
+         modalVisible: false,
+         zaffriModal: {
+             // notify | confirm
+             type: "notify",
+             title: "Notification",
+             messageBody: "This is just some example body text.",
+             confirmText: "Ok, close"
+         }
     },
     methods: {
         showModal: function() {
-            console.log('open');
             this.modalVisible = true;
         }
     }
