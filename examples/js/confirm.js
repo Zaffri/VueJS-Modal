4a74b68bc1ef06409550734fb0e1740553f8fb3e
Steven Morrison <stevenmorrimorrison@hotmail.co.uk>
2017-03-02 00:00:56 +0000

    SM: Extra component JS removed from examples.

--- examples/js/confirm.js
+++ examples/js/confirm.js
@@ -1,21 +1,10 @@
 /* --------------------------------------------------------
-	VueJS - Modal Component (with example)
+	VueJS - Modal Component
 	Version: 	0.0.2
 	Author: 	Steven Morrison
 	Website:	www.zaffri.com
 	GitHub:		github.com/Zaffri
 -------------------------------------------------------- */
-
-var ZaffriModal = Vue.component('zaffri-modal', {
-    template: "#zaffri-modal-template",
-    props: ['data'],
-    methods: {
-        closeModal: function(action = null) {
-            this.data.visible = false;
-            console.log("Modal action = " + action);
-        }
-    }
-});
 
 var app = new Vue({
     el: "#app",
