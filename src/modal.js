d6ab7854709ccfc15d876e2500452c624b9cf8e8
Steven Morrison <steven@zaffri.com>
2017-03-08 03:58:30 +0000

    SM: Passed callback data to callback.

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,6 @@
 /* --------------------------------------------------------
 	VueJS - Modal Component
-	Version: 	0.0.2
+	Version: 	0.0.3
 	Author: 	Steven Morrison
 	Website:	www.zaffri.com
 	GitHub:		github.com/Zaffri
@@ -11,11 +11,15 @@ var ZaffriModal = Vue.component('zaffri-modal', {
     props: ['data'],
     methods: {
         closeModal: function(action = null) {
+            var callbackData = {};
+
+            if(this.data.callbackData != undefined) callbackData = this.data.callbackData;
+            if(this.data.confirmCallback != undefined) this.confirmCallback(action, callbackData);
+
             this.data.visible = false;
-            if(this.data.confirmCallback != undefined) this.confirmCallback(action);
         },
-        confirmCallback: function(action) {
-            this.data.confirmCallback(action);
+        confirmCallback: function(action, callbackData) {
+            this.data.confirmCallback(action, callbackData);
         }
     }
 });
